0f614de5c5789b5bbb7ae3136bfd45102bd60c71
Minura Dinupriya <mranaweera793@gmail.com>
2023-12-08 13:50:36 +0530

    dao factory addded to customer and customerform is fully completed with layered architecture

---
+++ src/main/java/dao/DaoFactory.java
@@ -0,0 +1,17 @@
+package dao;
+
+import dao.custom.impl.CustomerDaoImpl;
+import dao.util.DaoType;
+
+public class DaoFactory {
+    private DaoFactory(){}
+    private static DaoFactory daoFactory;
+    public static DaoFactory getInstance(){
+        return daoFactory!=null? daoFactory:(daoFactory=new DaoFactory());
+    }
+    public <T extends SuperDao>T getDao(DaoType type){
+        switch (type){
+            case CUSTOMER:return (T)new CustomerDaoImpl();
+        }
+    }
+}
