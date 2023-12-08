1fe1979cfbd18fffa3b71f01fe9d41b74f3655ca
Minura Dinupriya <mranaweera793@gmail.com>
2023-12-08 07:28:57 +0530

    checked

---
+++ src/main/java/bo/SuperBo.java
@@ -0,0 +1,7 @@
+package bo;
+
+public class SuperBo {
+    public void hello(){
+        System.out.println("hello");
+    }
+}
---
+++ src/main/java/dao/OrderDetailsModel.java
@@ -0,0 +1,10 @@
+package dao;
+
+import dto.OrderDetailsDto;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface OrderDetailsModel {
+    boolean saveOrderDetails(List<OrderDetailsDto> list) throws SQLException, ClassNotFoundException;
+}
