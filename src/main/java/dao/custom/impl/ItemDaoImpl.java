3998e58c7b4041b71e12b180c9718fa9299bad85
Minura Dinupriya <mranaweera793@gmail.com>
2023-12-20 15:12:43 +0530

    item done with layered architecture

--- src/main/java/dao/custom/impl/ItemDaoImpl.java
+++ src/main/java/dao/custom/impl/ItemDaoImpl.java
@@ -1,5 +1,6 @@
 package dao.custom.impl;
 
+import dao.util.CrudUtil;
 import db.DBConnection;
 import dto.ItemDto;
 import dao.custom.ItemDao;
@@ -16,35 +17,28 @@ public class ItemDaoImpl implements ItemDao {
 
     @Override
     public Item getItem(String code) throws SQLException, ClassNotFoundException {
-        String sql = "SELECT * FROM item WHERE code=?";
-        PreparedStatement pstm = DBConnection.getInstance().getConnection().prepareStatement(sql);
-        pstm.setString(1,code);
-        ResultSet resultSet = pstm.executeQuery();
-        if (resultSet.next()){
-            return new Item(
-                    resultSet.getString(1),
-                    resultSet.getString(2),
-                    resultSet.getDouble(3),
-                    resultSet.getInt(4)
-            );
-        }
         return null;
     }
 
 
     @Override
     public boolean save(Item entity) throws SQLException, ClassNotFoundException {
-        String updateQuery="INSERT INTO item VALUES('"+entity.getCode()+"','"+entity.getDescription()+"','"+entity.getUnitPrice()+"','"+entity.getQtyOnHand()+"')";
-        Statement statement = DBConnection.getInstance().getConnection().createStatement();
-        return statement.executeUpdate(updateQuery)>0;
+        String updateQuery="INSERT INTO item VALUES(?,?,?,?)";
+        return CrudUtil.execute(
+                updateQuery,
+                entity.getCode(),
+                entity.getDescription(),
+                entity.getUnitPrice(),
+                entity.getQtyOnHand()
+        );
     }
 
     @Override
     public List<Item> getAll() throws SQLException, ClassNotFoundException {
         List<Item> list = new ArrayList<>();
         String sql = "SELECT * FROM item";
-        PreparedStatement pstm = DBConnection.getInstance().getConnection().prepareStatement(sql);
-        ResultSet resultSet = pstm.executeQuery();
+        ResultSet resultSet = CrudUtil.execute(sql);
+
         while (resultSet.next()){
             list.add(new Item(
                     resultSet.getString(1),
@@ -53,20 +47,30 @@ public class ItemDaoImpl implements ItemDao {
                     resultSet.getInt(4)
             ));
         }
+
         return list;
     }
 
     @Override
     public boolean update(Item entity) throws SQLException, ClassNotFoundException {
-        String updateQuery="UPDATE item SET code='"+entity.getCode()+"',description='"+entity.getDescription()+"',unitPrice="+entity.getUnitPrice()+", qtyOnHand="+entity.getQtyOnHand()+" WHERE code='"+entity.getCode()+"'";
-        Statement statement = DBConnection.getInstance().getConnection().createStatement();
-        return statement.executeUpdate(updateQuery)>0;
+        String updateQuery="UPDATE item SET description= ?, unitPrice=?, qtyOnHand=? WHERE code=?";
+
+        return CrudUtil.execute(
+                updateQuery,
+                entity.getDescription(),
+                entity.getUnitPrice(),
+                entity.getQtyOnHand(),
+                entity.getCode()
+        );
+
     }
 
     @Override
     public boolean delete(String value) throws SQLException, ClassNotFoundException {
-        String deleteQuery="DELETE FROM item WHERE code='"+value+"'";
-        Statement statement = DBConnection.getInstance().getConnection().createStatement();
-        return statement.executeUpdate(deleteQuery)>0;
+        String deleteQuery="DELETE FROM item WHERE code=?";
+        return CrudUtil.execute(
+                deleteQuery,
+                value
+        );
     }
 }
